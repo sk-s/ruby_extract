cb16915f16b4c50f9ec3d7592ea6fd763a2f8a63
HiroakiSakauchi <hiroaki@mba11hiro.local>
2012-07-30 06:51:58 +0900

    設定情報のシナリオ
    引数のパターンが不足しているので後で追加する。

---
+++ features/step_definitions/config_steps.rb
@@ -0,0 +1,10 @@
+#encoding: utf-8
+
+前提 /^存在する$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+もし /^SVN設定が正しい$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
